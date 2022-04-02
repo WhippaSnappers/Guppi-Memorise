db6fd8ebc899d94df2b17043e085e5f01331a094
OldestSalt <81101567+OldestSalt@users.noreply.github.com>
2022-04-02 20:43:12 +1000

    aaaaaaaaaaaa

--- Guppi-Memorise/Guppi-Memorise.Android/obj/Debug/120/android/environment.armeabi-v7a.s
+++ Guppi-Memorise/Guppi-Memorise.Android/obj/Debug/120/android/environment.armeabi-v7a.s
@@ -105,7 +105,7 @@ mono_aot_mode_name:
 	.section	.rodata.env.str.8,"aMS",%progbits,1
 	.type	.L.env.str.8, %object
 .L.env.str.8:
-	.asciz	"1c9def1d-598b-4263-b484-7224fa7bd3b7"
+	.asciz	"d7702a07-70a9-4913-bef0-af961abeb63f"
 	.size	.L.env.str.8, 37
 	.section	.rodata.env.str.9,"aMS",%progbits,1
 	.type	.L.env.str.9, %object
