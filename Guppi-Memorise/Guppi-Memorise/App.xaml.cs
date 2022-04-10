c9f46c67f021b944c9d1b7c24370d4594ac82f8e
OldestSalt <81101567+OldestSalt@users.noreply.github.com>
2022-04-10 22:30:02 +1000

    AAAAAAAAAAAAAAAA

--- Guppi-Memorise/Guppi-Memorise/App.xaml.cs
+++ Guppi-Memorise/Guppi-Memorise/App.xaml.cs
@@ -9,20 +9,8 @@ namespace Guppi_Memorise
         public App()
         {
             InitializeComponent();
-
-<<<<<<< HEAD
-            //var aboutapp = new NavigationPage(new AboutApp());
-            //MainPage = aboutapp;
             var navbar = new NavigationPage(new MainPage());
             MainPage = navbar;
-=======
-            var aboutapp = new NavigationPage(new MainPage());
-            MainPage = aboutapp;
-            //var navbar = new NavigationPage(new MainPage());
-            //MainPage = navbar;
->>>>>>> a2391196a1637a1adbac78d5a7d4a912bd500b6c
-            //navbar.BarBackgroundColor = Color.FromRgb(0, 205, 167);
-            //navbar.BarTextColor = Color.FromRgb(1, 37, 69);
         }
 
         protected override void OnStart()
