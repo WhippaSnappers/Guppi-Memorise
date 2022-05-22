493f5af1c6de7b73c07d36e1d32ff66049eb17a6
WhippaSnappers <24751261+WhippaSnappers@users.noreply.github.com>
2022-05-22 17:56:02 +1000

    Done working on DeckPage

--- Guppi-Memorise/Guppi-Memorise/DeckPage.xaml.cs
+++ Guppi-Memorise/Guppi-Memorise/DeckPage.xaml.cs
@@ -8,25 +8,33 @@ using System.Collections.ObjectModel;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
-namespace Guppi_Memorise {
+namespace Guppi_Memorise
+{
 	[XamlCompilation(XamlCompilationOptions.Compile)]
 
-    public partial class DeckPage : ContentPage {
+    public partial class DeckPage : ContentPage
+    {
 
         private Deck deck;
         private bool isRenaming = false;
         private bool isClicked = false;
         private ObservableCollection<Card> cards;
 
-        public DeckPage(Deck deck) {
+        public DeckPage(Deck deck)
+        {
             InitializeComponent();
             this.deck = deck;
-            BindableLayout.SetItemsSource(layout, cards);
             title.SetBinding(Label.TextProperty, new Binding { Source = deck, Path = "Name"});
-            Task.Run(async () =>
+            Task.Run(async () => await resetCollection());
+        }
+        private async Task resetCollection()
+        {
+            var cardsList = await DB.FetchCards(deck);
+            cardsList.Reverse();
+            cards = new ObservableCollection<Card>(cardsList);
+            Device.BeginInvokeOnMainThread(() =>
             {
-                var cardsList = await DB.FetchCards(deck);
-                cards = new ObservableCollection<Card>(cardsList);
+                BindableLayout.SetItemsSource(layout, cards);
                 var cardsCount = cardsList.Count;
                 if (cardsCount > 1)
                 {
@@ -35,110 +43,185 @@ namespace Guppi_Memorise {
                 }
             });
         }
-        private void CardTapped(object sender, MR.Gestures.TapEventArgs e) {
-            if (!isRenaming) {
-                var slChildren = (((sender as Frame).Content as AbsoluteLayout).Children[1] as StackLayout).Children;
-                slChildren[0].IsVisible = !slChildren[0].IsVisible;
-                slChildren[2].IsVisible = !slChildren[2].IsVisible;
+        private void CardTapped(object sender, MR.Gestures.TapEventArgs _)
+        {
+            if (!isRenaming)
+            {
+                var cardFrame = sender as Frame;
+                var absLayout = cardFrame.Content as AbsoluteLayout;
+                var stkLayout = absLayout.Children[1] as StackLayout;
+                var titleLabel = stkLayout.Children[0] as Label;
+                var textLabelSV = stkLayout.Children[2] as ScrollView;
+                
+                titleLabel.IsVisible = !titleLabel.IsVisible;
+                textLabelSV.IsVisible = !textLabelSV.IsVisible;
             }
         }
-        private async void AddCard(object sender, EventArgs e) {
-            if (!isRenaming) {
-                isRenaming = true;
-                var newCard = new Card() { DeckId = deck.Id};
-                await DB.AddCard(deck, newCard);
-                cards.Add(newCard);
-                var card = layout.Children.Where(i => Int32.Parse(i.ClassId) == newCard.Id).FirstOrDefault();
-                var slChildren = (((card as Frame).Content as AbsoluteLayout).Children[1] as StackLayout).Children;
-
-                if (cards.Count > 1) {
-                    selfControl.IsEnabled = true;
-                    sort.IsEnabled = true;
-                }
-
-                RenameCardToggle(slChildren);
-            }
-        }
-        private async void Frame_LongPressed(object sender, MR.Gestures.LongPressEventArgs e) {
-            if (!isRenaming) {
-                var slChildren = (((sender as Frame).Content as AbsoluteLayout).Children[1] as StackLayout).Children;
-                if (slChildren[0].IsVisible) {
-                    Card tappedCard = cards.Where(i => i.Id == Int32.Parse((sender as Frame).ClassId)).FirstOrDefault();
+        private async void CardLongPress(object sender, MR.Gestures.LongPressEventArgs _)
+        {
+            var cardFrame = sender as Frame;
+            var absLayout = cardFrame.Content as AbsoluteLayout;
+            var stkLayout = absLayout.Children[1] as StackLayout;
+            var titleLabel = stkLayout.Children[0] as Label;
+            if (!isRenaming)
+            {
+                if (titleLabel.IsVisible)
+                {
+                    Card tappedCard = cards.Where(i => i.Id == Int32.Parse(cardFrame.ClassId)).FirstOrDefault();
                     string res = await DisplayActionSheet("Выберите действие", "Отмена", "", "Удалить", "Переименовать");
-                    switch (res) {
+                    switch (res)
+                    {
                         case "Удалить":
-                            cards.Remove(tappedCard);
                             await DB.RemoveCard(tappedCard);
-                            if (cards.Count < 2) {
-                                selfControl.IsEnabled = false;
-                            }
+                            Device.BeginInvokeOnMainThread(() =>
+                            {
+                                cards.Remove(tappedCard);
+                                if (cards.Count < 2)
+                                {
+                                    selfControl.IsEnabled = false;
+                                    sort.IsEnabled = false;
+                                }
+                            });
                             break;
                         case "Переименовать":
                             isRenaming = true;
-                            RenameCardToggle(slChildren);
+                            var cardView = sender as Frame;
+                            RenameCardToggle(cardView);
                             break;
                     }
                 }
-                else {
+                else
+                {
                     isRenaming = true;
-                    ChangeTextToggle(slChildren);
+                    ChangeTextToggle(cardFrame);
                 }
             }
         }
-        private void RenameCardToggle(IList<View> slChildren) {
-            slChildren[0].IsVisible = !slChildren[0].IsVisible;
-            slChildren[1].IsVisible = !slChildren[1].IsVisible;
-            if (slChildren[1].IsVisible) {
-                (slChildren[1] as Editor).Focus();
+        private void AddCard(object sender, EventArgs _)
+        {
+            if (!isRenaming)
+            {
+                isRenaming = true;
+                // Improve threading (probably done)
+                var newCard = new Card();
+                Task.Run(async () =>
+                {
+                    await resetCollection();
+                    await DB.AddCard(deck, newCard);
+                    Device.BeginInvokeOnMainThread(() =>
+                    {
+                        cards.Insert(0, newCard);
+                        var card = layout.Children[0] as Frame;
+                        if (cards.Count > 1)
+                        {
+                            selfControl.IsEnabled = true;
+                            sort.IsEnabled = true;
+                        }
+                        RenameCardToggle(card);
+                    });
+                });
             }
         }
-        private void ChangeTextToggle(IList<View> slChildren) {
-            slChildren[2].IsVisible = !slChildren[2].IsVisible;
-            slChildren[3].IsVisible = !slChildren[3].IsVisible;
-            if (slChildren[3].IsVisible) {
-                ((slChildren[3] as ScrollView).Content as Editor).Focus();
+        private void RenameCardToggle(Frame cardFrame)
+        {
+            var absLayout = cardFrame.Content as AbsoluteLayout;
+            // var ratingLabel = absLayout.Children[0] as Label;
+            var stkLayout = absLayout.Children[1] as StackLayout;
+            var titleLabel = stkLayout.Children[0] as Label;
+            var titleEditor = stkLayout.Children[1] as Editor;
+            // var textLabel = (stkLayout.Children[2] as ScrollView).Content as Label;
+            // var textEditor = (stkLayout.Children[3] as ScrollView).Content as Editor;
+            titleLabel.IsVisible = !titleLabel.IsVisible;
+            titleEditor.IsVisible = !titleEditor.IsVisible;
+            if (titleEditor.IsVisible)
+            {
+                titleEditor.Focus();
             }
         }
-        private void Editor_Completed(object sender, EventArgs e) {
-            var slChildren = ((sender as Editor).Parent as StackLayout).Children;
-            RenameCardToggle(slChildren);
+        private void TitleEditorCompleted(object sender, EventArgs _)
+        {
+            var cardFrame = (((sender as Editor).Parent as StackLayout).Parent as AbsoluteLayout).Parent as Frame;
+            var idRaw = cardFrame.ClassId;
+            int id = int.Parse(idRaw);
+            Card curCard = cards.Where(i => i.Id == id).FirstOrDefault();
+            int cardIndex = cards.IndexOf(curCard);
+            var newTitle = (sender as Editor).Text;
+            cards[cardIndex].Title = newTitle;
+            curCard.Title = newTitle;
+            Task.Run(async () => await DB.UpdateCard(curCard));
+            var absLayout = cardFrame.Content as AbsoluteLayout;
+            var stkLayout = absLayout.Children[1] as StackLayout;
+            var titleLabel = stkLayout.Children[0] as Label;
+            titleLabel.Text = newTitle;
+            RenameCardToggle(cardFrame);
             isRenaming = false;
         }
-        private void Editor_Completed1(object sender, EventArgs e) {
-            var slChildren = (((sender as Editor).Parent as ScrollView).Parent as StackLayout).Children;
-            ChangeTextToggle(slChildren);
+        private void TextEditorCompleted(object sender, EventArgs _)
+        {
+            var cardFrame = ((((sender as Editor).Parent as ScrollView).Parent as StackLayout).Parent as AbsoluteLayout).Parent as Frame;
+            var absLayout = cardFrame.Content as AbsoluteLayout;
+            var stkLayout = absLayout.Children[1] as StackLayout;
+            var textLabel = (stkLayout.Children[2] as ScrollView).Content as Label;
+            var textEditor = (stkLayout.Children[3] as ScrollView).Content as Editor;
+            var idRaw = cardFrame.ClassId;
+            int id = int.Parse(idRaw);
+            Card curCard = cards.Where(i => i.Id == id).FirstOrDefault();
+            int cardIndex = cards.IndexOf(curCard);
+            var newText = textEditor.Text;
+            cards[cardIndex].Text = newText;
+            curCard.Text = newText;
+            Task.Run(async () => await DB.UpdateCard(curCard));
+            textLabel.Text = newText;
+            ChangeTextToggle(cardFrame);
             isRenaming = false;
         }
-        private void Button_Clicked(object sender, EventArgs e) {
-            if (!isClicked) {
+        private void ChangeTextToggle(Frame cardFrame)
+        {
+            var absLayout = cardFrame.Content as AbsoluteLayout;
+            var stkLayout = absLayout.Children[1] as StackLayout;
+            var textLabelSV = stkLayout.Children[2] as ScrollView;
+            var textEditorSV = stkLayout.Children[3] as ScrollView;
+
+            textLabelSV.IsVisible = !textLabelSV.IsVisible;
+            textEditorSV.IsVisible = !textEditorSV.IsVisible;
+
+            if (textEditorSV.IsVisible)
+            {
+                (textEditorSV.Content as Editor).Focus();
+            }
+        }
+        private void SelfControlButton(object sender, EventArgs _)
+        {
+            if (!isClicked)
+            {
                 isClicked = true;
                 Navigation.PushAsync(new SelfControlPage(cards));
                 isClicked = false;
             }
         }
-        private async void SortBtn(object sender, EventArgs e) {
+        private async void SortButton(object sender, EventArgs _)
+        {
             var result = await DisplayActionSheet("Отсортировать", "Отмена", "", "По рейтингу запоминания (по возрастанию)", "По рейтингу запоминания (по убыванию)", "По алфавиту (по возрастанию)", "По алфавиту (по убыванию)", "В порядке добавления");
-            switch (result) {
+            switch (result)
+            {
                 case "По рейтингу запоминания (по убыванию)":
                     cards = new ObservableCollection<Card>(cards.OrderByDescending(i => i.Rating));
-                    BindableLayout.SetItemsSource(layout, cards);
-                    BindableLayout.SetItemsSource(layout, cards);
+                    Device.BeginInvokeOnMainThread(() => BindableLayout.SetItemsSource(layout, cards));
                     break;
                 case "По рейтингу запоминания (по возрастанию)":
                     cards = new ObservableCollection<Card>(cards.OrderBy(i => i.Rating));
-                    BindableLayout.SetItemsSource(layout, cards);
+                    Device.BeginInvokeOnMainThread(() => BindableLayout.SetItemsSource(layout, cards));
                     break;
                 case "По алфавиту (по возрастанию)":
                     cards = new ObservableCollection<Card>(cards.OrderBy(i => i.Title));
-                    BindableLayout.SetItemsSource(layout, cards);
+                    Device.BeginInvokeOnMainThread(() => BindableLayout.SetItemsSource(layout, cards));
                     break;
                 case "По алфавиту (по убыванию)":
                     cards = new ObservableCollection<Card>(cards.OrderByDescending(i => i.Title));
-                    BindableLayout.SetItemsSource(layout, cards);
+                    Device.BeginInvokeOnMainThread(() => BindableLayout.SetItemsSource(layout, cards));
                     break;
                 case "В порядке добавления":
-                    cards = new ObservableCollection<Card>(cards.OrderByDescending(i => i.Id));
-                    BindableLayout.SetItemsSource(layout, cards);
+                    await resetCollection();
                     break;
             }
         }
--- Guppi-Memorise/Guppi-Memorise/FlashCardsPage.xaml.cs
+++ Guppi-Memorise/Guppi-Memorise/FlashCardsPage.xaml.cs
@@ -7,37 +7,45 @@ using System.Collections.ObjectModel;
 using System.Diagnostics;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
+using System.ComponentModel;
 
-namespace Guppi_Memorise {
+namespace Guppi_Memorise
+{
 	[XamlCompilation(XamlCompilationOptions.Compile)]
 
-    public partial class FlashCardsPage : ContentPage {
-
+    public partial class FlashCardsPage : ContentPage
+    {
         public static ObservableCollection<Deck> decks;
         private static bool isRenaming = false;
         private static bool isClicked = false;
 
-        public FlashCardsPage() {
+        public FlashCardsPage()
+        {
             InitializeComponent();
-            BindableLayout.SetItemsSource(layout, decks);
 
             Task.Run(async () =>
             {
                 var decksList = await DB.FetchDecks();
+                decksList.Reverse();
                 decks = new ObservableCollection<Deck>(decksList);
-
+                Device.BeginInvokeOnMainThread(() => BindableLayout.SetItemsSource(layout, decks));
             });
         }
-
-        private void RenameCard(object sender) {
+        private void RenameDeck(object sender)
+        {
             isRenaming = true;
             var sl = (sender as Frame).Content as StackLayout;
             (sl.Children[0] as Label).IsVisible = false;
             (sl.Children[1] as Editor).IsVisible = true;
             (sl.Children[1] as Editor).Focus();
         }
-
-        private void DeckTapped(object sender, MR.Gestures.TapEventArgs e) {
+        private void RemoveDeck(Deck deck)
+        {
+            decks.Remove(deck);
+            Task.Run(async () => await DB.RemoveDeck(deck));
+        }
+        private void DeckTapped(object sender, EventArgs _)
+        {
             if (!isRenaming) {
                 if (!isClicked) {
                     Deck tappedDeck = decks.Where(i => i.Id == Int32.Parse((sender as Frame).ClassId)).FirstOrDefault();
@@ -47,40 +55,55 @@ namespace Guppi_Memorise {
                 }
             }
         }
-
-        private async void AddDeck(object sender, EventArgs e) {
+        private void AddDeck(object sender, EventArgs e)
+        {
             if (!isRenaming) {
                 var newDeck = new Deck();
-                await DB.AddDeck(newDeck);
-                decks.Add(newDeck);
-
-
-
-                var deck = layout.Children.FirstOrDefault(i => Int32.Parse(i.ClassId) == newDeck.Id);
-                //RenameCard(newDeck);
+                Task.Run(async () =>
+                {
+                    await DB.AddDeck(newDeck);
+                    Device.BeginInvokeOnMainThread(() =>
+                    {
+                        decks.Insert(0, newDeck);
+                        var deck = layout.Children[0];
+                        RenameDeck(deck);
+                    });
+                });
             }
         }
-
-        private async void Frame_LongPressed(object sender, MR.Gestures.LongPressEventArgs e) {
+        private async void Frame_LongPressed(object sender, MR.Gestures.LongPressEventArgs e)
+        {
             if (!isRenaming) {
                 Deck tappedDeck = decks.Where(i => i.Id == Int32.Parse((sender as Frame).ClassId)).FirstOrDefault();
                 string res = await DisplayActionSheet("Выберите действие", "Отмена", "", "Удалить", "Переименовать");
                 switch (res) {
                     case "Удалить": 
-                        decks.Remove(tappedDeck);
+                        RemoveDeck(tappedDeck);
                         break;
                     case "Переименовать":
-                        RenameCard(sender);
+                        RenameDeck(sender);
                         break;
                 }
             }
         }
-
-        private void Editor_Completed(object sender, EventArgs e) {
-            isRenaming = false;
-            var sl = (sender as Editor).Parent as StackLayout;
-            (sl.Children[0] as Label).IsVisible = true;
-            (sl.Children[1] as Editor).IsVisible = false;
+        private void Editor_Completed(object sender, EventArgs e)
+        {
+            Device.BeginInvokeOnMainThread(() =>
+            {
+                string idRaw = (sender as Editor).ClassId;
+                int id = Int32.Parse(idRaw);
+                string newName = (sender as Editor).Text;
+                Deck curDeck = decks.Where(i => i.Id == id).FirstOrDefault();
+                int curDeckIndex = decks.IndexOf(curDeck);
+                decks[curDeckIndex].Name = newName;
+                curDeck.Name = newName;
+                Task.Run(async () => await DB.UpdateDeck(curDeck));
+                var sl = (sender as Editor).Parent as StackLayout;
+                (sl.Children[0] as Label).Text = newName;
+                (sl.Children[0] as Label).IsVisible = true;
+                (sl.Children[1] as Editor).IsVisible = false;
+                isRenaming = false;
+            });
         }
     }
 }
