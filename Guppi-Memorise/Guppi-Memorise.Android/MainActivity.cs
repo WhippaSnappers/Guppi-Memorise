49303ff0263a509ec929ff307fa61955ea5a9e91
WhippaSnappers <24751261+WhippaSnappers@users.noreply.github.com>
2022-06-05 18:13:37 +1000

    Wrapping up

--- Guppi-Memorise/Guppi-Memorise.Android/MainActivity.cs
+++ Guppi-Memorise/Guppi-Memorise.Android/MainActivity.cs
@@ -7,7 +7,7 @@ using Android.OS;
 
 namespace Guppi_Memorise.Droid
 {
-    [Activity(Label = "Guppi_Memorise", Icon = "@mipmap/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation | ConfigChanges.UiMode | ConfigChanges.ScreenLayout | ConfigChanges.SmallestScreenSize )]
+    [Activity(Label = "Guppi Memorise", Icon = "@drawable/guppi", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation | ConfigChanges.UiMode | ConfigChanges.ScreenLayout | ConfigChanges.SmallestScreenSize )]
     public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
     {
         protected override void OnCreate(Bundle savedInstanceState)
